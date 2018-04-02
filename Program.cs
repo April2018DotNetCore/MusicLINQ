cc42a4a0f3d87ea6a502cb3f4f1e81a6764bb356
Noelle <noelle.teh@gmail.com>
2018-04-02 14:26:19 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MusicLINQ
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Collections to work with
+            List<Artist> Artists = JsonToFile<Artist>.ReadJson();
+            List<Group> Groups = JsonToFile<Group>.ReadJson();
+
+            //=========================================================
+            //Solve all of the prompts below using various LINQ queries
+            //=========================================================
+
+            //=========================================================
+            //There is only one artist in this collection from Mount 
+            //Vernon. What is their name and age?
+            //=========================================================
+
+            //=========================================================
+            //Who is the youngest artist in our collection of artists?
+            //=========================================================
+
+            //=========================================================
+            //Display all artists with 'William' somewhere in their 
+            //real name        
+            //=========================================================
+
+            //=========================================================
+            //Display all groups with names less than 8 characters
+            //=========================================================
+
+            //=========================================================
+            //Display the 3 oldest artists from Atlanta
+            //=========================================================
+            
+            //=========================================================
+            //(Optional) Display the Group Name of all groups that have 
+            //at least one member not from New York City
+            //=========================================================
+
+            //=========================================================
+            //(Optional) Display the artist names of all members of the 
+            //group 'Wu-Tang Clan'
+            //=========================================================
+        }
+    }
+}
